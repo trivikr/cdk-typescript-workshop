4c0ecd0e18494ece6489f37a7d2cd4d71e393615
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-03-07 03:41:17 +0000

    feat: add lambda behind API Gateway

--- lib/test-stack.ts
+++ lib/test-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class TestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,6 +10,10 @@ export class TestStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_12_X,
       code: lambda.Code.fromAsset("lambda"),
       handler: "hello.handler"
+    });
+
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello
     });
   }
 }
