b950bd4b02a9a04816d3c2ee096e241c34619afe
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-03-07 02:34:00 +0000

    chore: add prettier

--- bin/test.ts
+++ bin/test.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { TestStack } from '../lib/test-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { TestStack } from "../lib/test-stack";
 
 const app = new cdk.App();
-new TestStack(app, 'TestStack');
+new TestStack(app, "TestStack");
