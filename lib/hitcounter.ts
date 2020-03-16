02ba650fd4e3c21265e1eb6fc65df88d4a40b0ec
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-03-16 04:54:06 +0000

    feat: use cdk-dynamo-table-viewer

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -11,10 +11,13 @@ export class HitCounter extends cdk.Construct {
   /** allows accessing the counter function */
   public readonly handler: lambda.Function;
 
+  /** the hit counter table */
+  public readonly table: dynamodb.Table;
+
   constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
-    const table = new dynamodb.Table(this, "Hits", {
+    this.table = new dynamodb.Table(this, "Hits", {
       partitionKey: { name: "path", type: dynamodb.AttributeType.STRING }
     });
 
@@ -24,12 +27,12 @@ export class HitCounter extends cdk.Construct {
       code: lambda.Code.fromAsset("lambda"),
       environment: {
         DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
-        HITS_TABLE_NAME: table.tableName
+        HITS_TABLE_NAME: this.table.tableName
       }
     });
 
     // grant the lambda role read/write permissions to our table
-    table.grantReadWriteData(this.handler);
+    this.table.grantReadWriteData(this.handler);
 
     // grant the lambda role invoke permissions to the downstream function
     props.downstream.grantInvoke(this.handler);
--- lib/test-stack.ts
+++ lib/test-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as apigw from "@aws-cdk/aws-apigateway";
 import { HitCounter } from "./hitcounter";
+import { TableViewer } from "cdk-dynamo-table-viewer";
 
 export class TestStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -19,6 +20,11 @@ export class TestStack extends cdk.Stack {
 
     new apigw.LambdaRestApi(this, "Endpoint", {
       handler: helloWithCounter.handler
+    });
+
+    new TableViewer(this, "ViewHitCounter", {
+      title: "Hello Hits",
+      table: helloWithCounter.table
     });
   }
 }
